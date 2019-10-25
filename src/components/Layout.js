f7656d9c4b776138e8f3eda48b87cd6d24597671
Anonymous <mcfarlrose@gmail.com>
2019-10-25 13:07:26 -0700

    added favicons <3

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,6 +15,9 @@ export default class Body extends React.Component {
                     <title>{_.get(this.props, 'pageContext.frontmatter.title') && _.get(this.props, 'pageContext.frontmatter.title') + ' - '}{_.get(this.props, 'pageContext.site.siteMetadata.title')}</title>
                     <meta charSet="utf-8" />
                     <meta name="viewport" content="width=device-width, initialScale=1, userScalable=no" />
+                    <link rel="icon" type="image/png" sizes="16x16" href={safePrefix('images/favicon-16x16.png')} />
+                    <link rel="icon" type="image/png" sizes="60x60" href={safePrefix('images/favicon-60x60.png')} />
+                    <link rel="icon" type="image/png" sizes="32x32" href={safePrefix('images/favicon-32x32.png')} />
                     <link rel="stylesheet" href={safePrefix('assets/css/main.css')} />
                     <link rel="stylesheet" href={safePrefix('assets/css/markdown-images.css')} />
                     <noscript>{`<link rel="stylesheet" href=${safePrefix('assets/css/noscript.css')} />`}</noscript>
